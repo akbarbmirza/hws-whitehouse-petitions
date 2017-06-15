cbeed4e9d41d2fbf3c9b852f057ec0e80d29d785
Akbar Mirza <akbarbmirza@gmail.com>
2017-06-14 23:51:25 -0400

    Add Basic UI: TabBarController and TableView

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    // =========================================================================
+    // Outlets
+    // =========================================================================
+    
+    // =========================================================================
+    // Properties
+    // =========================================================================
+    
+    // create a variable to hold our petitions
+    var petitions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // =========================================================================
+    // TableView Methods
+    // =========================================================================
+    
+    // tells us how many rows will be in our view
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    // tells iOS how to set up each individual cell in our table
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // dequeue the cell in our storyboard
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // set the labels in our cell
+        cell.textLabel?.text = "Title Goes Here"
+        cell.detailTextLabel?.text = "Subtitle Goes Here"
+        
+        // return our cell
+        return cell
+    }
 
 }
 
