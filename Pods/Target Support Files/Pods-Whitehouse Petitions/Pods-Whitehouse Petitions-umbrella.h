56099abd44154e089658b206eee4f36bfc535aa2
Akbar Mirza <akbarbmirza@gmail.com>
2017-06-18 12:56:52 -0400

    Install SwiftJSON cocoa pod

---
+++ Pods/Target Support Files/Pods-Whitehouse Petitions/Pods-Whitehouse Petitions-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Whitehouse_PetitionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Whitehouse_PetitionsVersionString[];
+
