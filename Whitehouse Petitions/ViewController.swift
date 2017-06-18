1795fc8ce9811a9df50f01e870ddb3b498fe93d1
Akbar Mirza <akbarbmirza@gmail.com>
2017-06-18 13:13:06 -0400

    load and parse JSON data from whitehouse petitions API

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON // to handle JSON
 
 class ViewController: UITableViewController {
     
@@ -19,11 +20,25 @@ class ViewController: UITableViewController {
     // =========================================================================
     
     // create a variable to hold our petitions
-    var petitions = [String]()
+    var petitions = [[String: String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+                
+                // if our response code is 200 (i.e. everything is normal)
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    // we're OK to parse!
+                    parse(json: json)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,12 +61,41 @@ class ViewController: UITableViewController {
         // dequeue the cell in our storyboard
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        // get the petition data for this cell
+        let petition = petitions[indexPath.row]
+        
         // set the labels in our cell
-        cell.textLabel?.text = "Title Goes Here"
-        cell.detailTextLabel?.text = "Subtitle Goes Here"
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
         
         // return our cell
         return cell
+    }
+    
+    // =========================================================================
+    // Helper Methods
+    // =========================================================================
+    
+    // reads the "results" array from the JSON object it gets passed and adds it
+    // to our petitions property
+    func parse(json: JSON) {
+        // get our json array
+        for result in json["results"].arrayValue {
+            // store the title, body, and sigs in a dictionary that we can use
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = [
+                "title": title,
+                "body": body,
+                "sigs": sigs
+            ]
+            // append that dictionary to our petitions object
+            petitions.append(obj)
+        }
+        
+        // reload our tableview
+        tableView.reloadData()
     }
 
 }
