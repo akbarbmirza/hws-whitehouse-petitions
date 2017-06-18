727fb8965357baf4d920c9574923f84a9c2e06f8
Akbar Mirza <akbarbmirza@gmail.com>
2017-06-18 13:26:49 -0400

    render petitions detail view using WKWebView

--- Whitehouse Petitions/Controllers/DetailViewController.swift
+++ Whitehouse Petitions/Controllers/DetailViewController.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
+import WebKit // for WKWebView
 
 class DetailViewController: UIViewController {
-
+    
+    // =========================================================================
+    // Outlets
+    // =========================================================================
+    
+    // =========================================================================
+    // Properties
+    // =========================================================================
+    
+    // variable that references the WebView we'll be using
+    var webView: WKWebView!
+    // contains the dictionary of petition data
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // only continue if detailItem exists
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            // set the html that our webView is going to load
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            
+            // load the html that we specified
+            webView.loadHTMLString(html, baseURL: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +59,4 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
